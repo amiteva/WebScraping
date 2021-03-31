6249f7deb49de92b737ca9c98f23a1b508f20da3
Aleksandra Miteva <a.miteva@outlook.com>
2021-03-31 13:35:51 +0200

    Changed input from standard io to command-line arguments

--- src/main/java/com/webscraping/WebScrape.java
+++ src/main/java/com/webscraping/WebScrape.java
@@ -1,7 +1,6 @@
 package com.webscraping;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -21,17 +20,14 @@ public class WebScrape {
             Elements searchresults = doc.getElementsByClass("search-item");
 
             //If there are 0 results from the search
-            if(searchresults.size() == 0) {
+            if (searchresults.size() == 0) {
                 return "The player is not in our database";
             }
 
-            //Go through all of the displayed players
-            for(Element result : searchresults) {
-                link = result.getElementsByClass("search-item-url").text();
-                break; //we always take the first result
-            }
-
+            //We always take the first result from the search
+            link = searchresults.get(0).getElementsByClass("search-item-url").text();
             link = "https://www.basketball-reference.com" + link;
+
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -46,17 +42,17 @@ public class WebScrape {
             //so instead, I create a new document that contains only our wanted table and extract the information from there
             Node child = table.childNode(4);
             String s = child.toString();
-            String subs = s.substring(6, s.length()-4);
+            String subs = s.substring(6, s.length() - 4);
             Document doc1 = Jsoup.parse(subs);
             Element table1 = doc1.getElementById("per_minute");
 
             Element t = table1.getElementsByTag("tbody").get(0);
-            Elements rows =  t.getElementsByTag("tr");
+            Elements rows = t.getElementsByTag("tr");
 
             //Go through all of the rows of the table and print the wanted data
-            for(Element row : rows) {
-                Elements seasons = row.getElementsByAttributeValue("data-stat","season");
-                Elements points = row.getElementsByAttributeValue("data-stat","fg3a_per_mp");
+            for (Element row : rows) {
+                Elements seasons = row.getElementsByAttributeValue("data-stat", "season");
+                Elements points = row.getElementsByAttributeValue("data-stat", "fg3a_per_mp");
                 output = output.concat(seasons.text() + " " + points.text() + "\n");
             }
 
@@ -68,8 +64,12 @@ public class WebScrape {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine(); //take the name of the player from standard input
-        System.out.print(get3PA(input));
+        try {
+            String player_name = String.format("%s %s", args[0], args[1]);
+            System.out.println(player_name);
+            System.out.println(get3PA(player_name));
+        } catch (Exception e) {
+            System.out.println("Please enter a name and surname of a player!");
+        }
     }
 }
